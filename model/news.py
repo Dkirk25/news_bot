1d7869580245b12da0d4ac5b396c3f4ed2e81b24
Donald Kirk <dkirk9325@gmail.com>
2020-08-13 22:29:59 -0500

    updated configurations. modified the scheduler.

--- model/news.py
+++ model/news.py
@@ -1,10 +1,12 @@
 class StockInfo:
-    def __init__(self, title, author, text, url):
+    def __init__(self, uuid, title, author, published_at, text, url):
+        self.uuid = uuid
         self.title = title
         self.author = author
+        self.published_at = published_at
         self.text = text
         self.url = url
 
     def __str__(self):
-        return 'title= {self.title}\nauthor= {self.author}\ntext= {self.text}\nurl= {self.url}\n'.format(
+        return 'uuid= {self.uuid}\ntitle= {self.title}\nauthor= {self.author}\npublished_at= {self.published_at}\ntext= {self.text}\nurl= {self.url}\n'.format(
             self=self)
--- stock_news.py
+++ stock_news.py
@@ -1,41 +1,43 @@
 from pyrh import Robinhood
 from datetime import datetime
 from model.news import StockInfo
+import sys
 import numpy as np
 import tulipy as ti
-import sched
+import schedule
 import time
 import pyotp
 import os
 from dotenv import load_dotenv
-load_dotenv()
+load_dotenv(override=True)
 
+MFA = os.getenv("MFA")
+USERNAME = os.getenv("USERNAME")
+PASSWORD = os.getenv("PASSWORD")
 
-totp = pyotp.TOTP(os.getenv("MFA")).now()
+totp = pyotp.TOTP(MFA).now()
 rh = Robinhood()
-rh.login(username=os.getenv("USERNAME"),
-         password=os.getenv("PASSWORD"),
-         qr_code=os.getenv("MFA"))
+rh.login(username=USERNAME,
+         password=PASSWORD,
+         qr_code=MFA)
+
 # Initiate our scheduler so we can keep checking every minute for new price changes
-s1 = sched.scheduler(time.time, time.sleep)
+# s1 = sched.scheduler(time.time, time.sleep)
 
 
-def fetch_news(sc1):
+def fetch_news():
     print("Getting news\n")
-
-    # incorporate discord
-
-    # Have news be sent to "stock-news" channel in chat
 
     news = rh.get_news("WKHS")
     info_results = news["results"]
     for i in info_results:
-        stock_info = StockInfo(i["title"], i["source"],
+        stock_info = StockInfo(i["uuid"], i["title"], i["source"], i["published_at"],
                                i["preview_text"].replace("\n\n", ""), i["url"])
         print(str(stock_info))
 
-    s1.enter(300, 1, fetch_news, (sc1, ))
 
+schedule.every().minute.at(":10").do(fetch_news)
 
-s1.enter(1, 1, fetch_news, (s1, ))
-s1.run()
+while True:
+    schedule.run_all()
+    # time.sleep(1)
