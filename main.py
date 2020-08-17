394a83919907e24d19c560dc51c2cf07b0301f84
Donald Kirk <dkirk9325@gmail.com>
2020-08-17 16:50:07 -0500

    Refactored and implemented looping event for bot.

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ rh.login(username=USERNAME,
 # https://www.youtube.com/watch?v=rWAnKvI2ePI
 
 
-def get_channel(channels, channel_name):
+async def get_channel(channels, channel_name):
     for channel in client.get_all_channels():
         # print(channel)
         if channel.name == channel_name:
@@ -37,7 +37,7 @@ def get_channel(channels, channel_name):
     return None
 
 
-def fetch_news():
+async def fetch_news():
     print("Getting news\n")
 
     news = rh.get_news("WKHS")
@@ -51,55 +51,58 @@ def fetch_news():
     return clean_stock_list
 
 
-@client.event
-async def on_ready():
-    print("Bot is ready")
-    await client.wait_until_ready()
-
-    channel = get_channel(client.get_all_channels(), 'general')
-
-    # Call stock news and return list of stock info
-    stock_info_list = fetch_news()
-
-    # Read history of discord chat and save the last 10 messages
-    messages = await channel.history(limit=5).flatten()
-
-    news_bot_messages = []
-    for message in messages:
-        if(message.author.name == "news-bot"):
-            news_bot_messages.append(message)
-
-    # check last article published_at by the bot
-    latest_discord_message = news_bot_messages[0]
-    print(latest_discord_message)
-
-    # latest discord message is older than stock
-    latest_stock = stock_info_list[0]
-    clean_date = latest_stock.published_at.replace(
+async def get_clean_date(dirty_date):
+    clean_date = dirty_date.replace(
         "-", "/").replace("T", " ").replace("Z", "")
     print(clean_date)
     stock_date = datetime.strptime(
         clean_date, '%Y/%m/%d %H:%M:%S')
+    return stock_date
 
-    if(stock_date > latest_discord_message.created_at):
+
+async def post_news_info():
+    await client.wait_until_ready()
+    while(True):
+        channel = await get_channel(client.get_all_channels(), 'general')
+
+        # Call stock news and return list of stock info
+        stock_info_list = await fetch_news()
+
+        # Read history of discord chat and save the last 10 messages
+        messages = await channel.history(limit=10).flatten()
+
+        news_bot_messages = []
+        [news_bot_messages.append(
+            message) for message in messages if message.author.name == "news-bot"]
+
+        # check last article published_at by the bot
+        latest_discord_message = news_bot_messages[0]
+        print(latest_discord_message)
+
+        # latest discord message is older than stock
+        latest_stock = stock_info_list[0]
+        clean_date = await get_clean_date(latest_stock.published_at)
+
         # if any of the published_at in stock info list is newer than the last message in discord chat, then post stock info in chat
+        if(clean_date <= latest_discord_message.created_at):
+            # Create embed then post stock info
+            embed = discord.Embed(
+                title=latest_stock.title,
+                description=latest_stock.text,
+                url=latest_stock.url
+            )
+            embed.set_author(name=latest_stock.author)
+            # Send stock news to discord channel
+            await channel.send(embed=embed)
+            # Play every # of seconds
+            await asyncio.sleep(10)
 
-        # Create embed then post stock info
-        embed = discord.Embed(
-            title=latest_stock.title,
-            description=latest_stock.text,
-            url=latest_stock.url
-        )
-        embed.set_author(name=latest_stock.author)
-        # Send stock news to discord channel
-        await channel.send(embed=embed)
+
+@client.event
+async def on_ready():
+    print("Bot is ready")
+    await post_news_info()
 
 
+# client.loop.create_task(post_news_info())
 client.run(os.getenv("TOKEN"))
-
-# schedule.every(1).minutes.do(fetch_news)
-# schedule.every().minute.at("1:00").do(fetch_news)
-
-# while True:
-#     schedule.run_all()
-# time.sleep(1)
