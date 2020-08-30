b26374af19fb3cf3d7726d32420dcb06766c4d6e
Donald Kirk <dkirk9325@gmail.com>
2020-08-30 15:51:37 -0500

    Added firebase database.

---
+++ database.py
@@ -0,0 +1,35 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+# Use a service account
+cred = credentials.Certificate('./news-bot-key.json')
+firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+
+
+def create_collection():
+    doc_ref = db.collection(u'stocks').document(u'WKHS')
+    doc_ref.set({
+        u'name': u'WKHS',
+    })
+
+
+def add_stock(stock_name):
+    doc_ref = db.collection(u'stocks').document(stock_name)
+    doc_ref.set({
+        u'name': stock_name,
+    })
+
+
+def get_stocks():
+    stocks_ref = db.collection(u'stocks')
+    docs = stocks_ref.stream()
+
+    list_of_stocks = []
+    for doc in docs:
+        list_of_stocks.append(doc.id)
+        print(f'{doc.id} => {doc.to_dict()}')
+    return list_of_stocks
+    # print(f'{doc.id} => {doc.to_dict()}')
