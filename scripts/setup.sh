04ba2ca0a5b135249ec14fd4d157488e08f19e10
Donald Kirk <dkirk9325@gmail.com>
2020-09-17 20:47:27 -0500

    more tweaks

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-cd ..
 ## Configure Dev machine (Docker Image)
 touch .env
 touch news-bot-key.json
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-cd ..
 docker build . -t newsbotapp
 docker run newsbotapp:latest
