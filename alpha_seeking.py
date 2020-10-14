53b96dc10e3af0e6943c7b9a2043ceeb783b40be
Donald Kirk <dkirk9325@gmail.com>
2020-10-13 23:55:58 -0500

    scrapes alpha site.

--- alpha_seeking.py
+++ alpha_seeking.py
@@ -1,29 +1,36 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
 import chromedriver_binary
+import json
+import time
 
-driver = webdriver.Chrome()
+op = webdriver.ChromeOptions()
+op.add_argument('headless')
+driver = webdriver.Chrome(options=op)
 
 html = driver.page_source
 
-PARSER = 'html.parser'
-
-
 def get_news(stock):
+    time.sleep(30)
     url = 'https://seekingalpha.com/symbol/'+ stock +'/news'
     driver.get(url)
     html = driver.page_source
     soup = BeautifulSoup(html,features="lxml")
 
     # Get list of atricles
-    dirty_news_articles =  soup.find_all(attrs={"class": "a36d8-2-Y79"})[0]
-    latest_news_article = []
+    dirty_news_article =  soup.find_all(attrs={"class": "a36d8-2-Y79"})[0]
+    data = {}
 
-    for article in dirty_news_articles:
-        # Clean the response
-         
-        
-        # add it to a new list
-        latest_news_article.append(article.text)
-
-    return latest_news_article[0]
+    data['uuid'] = "None"
+    #Title
+    data['title'] = soup.find(attrs={"class": "_66147-2pxC0 _7e758-JdZFq _7e758-SfA3O _7e758-1Ou5t"}).contents[0]
+    #Description
+    data['preview_text'] = dirty_news_article.contents[0].next_element.text
+    # Source
+    data['source'] = dirty_news_article.contents[0].next_sibling.next.text
+    # published_at
+    data['published_at'] = dirty_news_article.contents[0].next_sibling.next.next_sibling.text
+    # URL
+    data['url'] = dirty_news_article.contents[0].next_element.attrs['href']  
+    # add it to a new list
+    return json.loads(json.dumps(data))
--- alpha_seeking_test.py
+++ alpha_seeking_test.py
@@ -6,11 +6,10 @@ import alpha_seeking
 class TestSum(unittest.TestCase):
 
     def test_fetch_WHKS_news(self):
-        news = []
         news = alpha_seeking.get_news("WKHS")
 
         print(news)
-        self.assertEqual("Workhorse -9% with USPS said to delaying big contract decision", news)
+        self.assertEqual("Workhorse Group Inc.", news["title"])
 
 
 if __name__ == '__main__':
